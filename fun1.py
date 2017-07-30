9ca6fbd3f7d2c711f81ca5458a801903d40f697e
jennifer19-meet <jennifer19@meet.mit.edu>
2017-07-30 12:54:41 +0200

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,15 @@
+def add_numbers (start, end) :
+    c = 0
+    for number in range (start, end + 1) :
+        c=c+number
+    return (c)
+    
+
+test1 = add_numbers (1,2)
+print (test1)
+test2 = add_numbers (1, 100)
+print (test2)
+test3 = add_numbers (1000, 5000)
+print (test3)
+##answer = add_numbers (start, end)
+##print (answer)
---
+++ fun2.py
@@ -0,0 +1,4 @@
+def draw_1d (n, c1) :
+    a = "*"* n
+    print (a)
+
